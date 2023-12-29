a94729eb8736af0f8ca0bced42897e953deb3e35
henkdegroot <13550012+henkdegroot@users.noreply.github.com>
2023-12-29 12:15:06 +0100

    localeupd

--- Clock/Config.py
+++ Clock/Config.py
@@ -34,7 +34,7 @@ digital = 1  # 1 = Digital Clock, 0 = Analog Clock
 
 # Goes with light blue config (like the default one)
 #digitalcolor = '#50CBEB'
-digitalcolor = '#40A3BD'
+digitalcolor = '#238199'
 #digitalformat = '{0:%I:%M\n%S %p}'  # Format of the digital clock face
 digitalformat = '{0:%H:%M}'  # my version
 digitalsize = 250
@@ -112,7 +112,7 @@ Language = 'NL'
 # locale -a
 # to install locales
 # sudo dpkg-reconfigure locales
-DateLocale = 'nl'
+DateLocale = 'nl_NL.UTF-8'
 
 # Language specific wording
 LPressure = 'Luchtdruk '
